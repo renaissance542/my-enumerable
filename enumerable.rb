8ceba1c2332046b7dd6d0d5751f26964f944cec5
Philippe Roy <renaissance542@gmail.com>
2021-11-22 12:43:31 -0500

    Methods all working with arrays, procs, and block.  Not yet working with hashes.

--- enumerable.rb
+++ enumerable.rb
@@ -7,10 +7,8 @@ module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
 
-    i = 0
-    until i == length
+    length.times do |i|
       yield self[i]
-      i += 1
     end
     self
   end
@@ -35,31 +33,76 @@ module Enumerable
   def my_all?(pattern = nil)
     if block_given?
       my_each { |i| return false unless yield i }
-      true
-    elsif pattern.nil?
-      false
-    else # pattern w/o block
-      my_each { |i| return false unless i in pattern }
+    elsif !pattern.nil?
+      my_each { |i| return false unless pattern === i}
+    else  # default block if no block or no pattern passed
+      my_each { |i|return false unless i }
     end
-  end
-
-  def my_any?
-    my_each { |i| return true if yield i }
-    false
-  end
-
-  def my_none?
-    my_each { |i| return false if yield i }
     true
   end
 
-  # def match_pattern(obj, pattern)
-  #   case obj
-  #     in ^pattern
-  #     true
-  #   else
-  #     false
-  #   end 
-  # end
+  def my_any?(pattern = nil)
+    if block_given?
+      my_each { |i| return true if yield i }
+    elsif !pattern.nil?
+      my_each { |i| return true if pattern === i }
+    else # default block if no block or no pattern passed
+      my_each { |i|return true if i }
+    end
+    false
+  end
 
+  def my_none?(pattern = nil)
+    if block_given?
+      my_each { |i| return false if yield i }
+    elsif !pattern.nil?
+      my_each { |i| return false if pattern === i }
+    else # default block if no block or no pattern passed
+      my_each { |i| return false if i }
+    end
+    true
+  end
+
+  def my_count(obj = nil, &block)
+    if block_given?
+      my_select(&block).length
+    elsif !obj.nil?
+      (my_select { |i| i == obj }).length
+    else
+      length
+    end
+  end
+
+  # map { |obj| block } → array
+  # map → an_enumerator
+  # Returns a new array with the results of running block once for every element in enum.
+  # If no block is given, an enumerator is returned instead.
+  def my_map(proc = nil)
+    return to_enum(:my_map) unless block_given? || !proc.nil?
+
+    arr = []
+    if proc.nil?
+      my_each_with_index { |obj, ind| arr[ind] = yield obj }
+    else
+      my_each_with_index { |obj, ind| arr[ind] = proc.call obj }
+    end
+    arr
+  end
+
+  # inject(initial, sym) → obj
+  # inject(sym) → obj
+  # inject(initial) { |memo, obj| block } → obj
+  # inject { |memo, obj| block } → obj
+  def my_inject(*args)
+    memo = self.first
+    args.each do |arg|
+      if arg.instance_of?(Symbol)
+        my_each { |i| memo = memo.method(arg).(i) }
+      else
+        memo = arg
+      end
+    end
+    my_each { |i| memo = yield memo, i } if block_given?
+    memo
+  end
 end
--- main.rb
+++ main.rb
@@ -48,35 +48,166 @@ system "clear"
 # p(arr.my_select { |i| i.even? })
 
 
-arr = ['a', 2, 'a']
+
+# puts "\n   *** ALL? ***"
+# puts '["a", 2, "a"].all? { |i| i.is_a? Integer }'
+# p(['a', 2, 'a'].all? { |i| i.is_a? Integer })
+# puts '["a", 2, "a"].my_all? { |i| i.is_a? Integer }'
+# p(['a', 2, 'a'].my_all? { |i| i.is_a? Integer })
+
+# puts "\n"
+# puts '[1, 2, 3].all? { |i| i.is_a? Integer }'
+# p([1, 2, 3].all? { |i| i.is_a? Integer })
+# puts '[1, 2, 3].my_all? { |i| i.is_a? Integer }'
+# p([1, 2, 3].my_all? { |i| i.is_a? Integer })
+
+# puts "\n"
+# puts '[1, "c", nil].all?'
+# p ([1, 'c', nil].all?)
+# puts '[1, "c", nil].my_all?'
+# p ([1, 'c', nil].my_all?)
+
+# puts "\n"
+# puts '[1, "c", 3].all?'
+# p ([1, 'c', 3].all?)
+# puts '[1, "c", 3].my_all?'
+# p ([1, 'c', 3].my_all?)
+
+# puts "\n   *** ANY? ***"
+# puts '[1, 2, "c"].any? { |i| i.is_a? Integer }'
+# puts '=>' + ([1, 2, 'c'].any? { |i| i.is_a? Integer }).to_s
+# puts '[1, 2, "c"].my_any? { |i| i.is_a? Integer }'
+# puts '=>' + ([1, 2, 'c'].my_any? { |i| i.is_a? Integer }).to_s
+
+# puts "\n"
+# puts '["a", "b", "c"].any? { |i| i.is_a? Integer }'
+# puts '=>' + (["a", "b", "c"].any? { |i| i.is_a? Integer }).to_s
+# puts '["a", "b", "c"].my_any? { |i| i.is_a? Integer }'
+# puts '=>' + (["a", "b", "c"].my_any? { |i| i.is_a? Integer }).to_s
+
+# puts "\n"
+# puts '["a", "b", "c"].any? { |i| i.is_a? String }'
+# puts '=>' + (["a", "b", "c"].any? { |i| i.is_a? String }).to_s
+# puts '["a", "b", "c"].my_any? { |i| i.is_a? String }'
+# puts '=>' + (["a", "b", "c"].my_any? { |i| i.is_a? String }).to_s
+
+# puts "\n"
+# puts '[nil, false, 3].any? '
+# puts '=>' + ([nil, false, 3].any?).to_s
+# puts '[nil, false, 3].my_any? '
+# puts '=>' + ([nil, false, 3].my_any?).to_s
+
+# puts "\n"
+# puts '[nil, false].any? '
+# puts '=>' + ([nil, false].any?).to_s
+# puts '[nil, false].my_any? '
+# puts '=>' + ([nil, false].my_any?).to_s
+
+
+# puts "\n   *** NONE? ***"
+# puts '[1, 2, "c"].none? { |i| i.is_a? Integer }'
+# puts '=>' + ([1, 2, 'c'].none? { |i| i.is_a? Integer }).to_s
+# puts '[1, 2, "c"].my_none? { |i| i.is_a? Integer }'
+# puts '=>' + ([1, 2, 'c'].my_none? { |i| i.is_a? Integer }).to_s
+
+# puts "\n"
+# puts '["a", "b", "c"].none? { |i| i.is_a? Integer }'
+# puts '=>' + (["a", "b", "c"].none? { |i| i.is_a? Integer }).to_s
+# puts '["a", "b", "c"].my_none? { |i| i.is_a? Integer }'
+# puts '=>' + (["a", "b", "c"].my_none? { |i| i.is_a? Integer }).to_s
+
+# puts "\n"
+# puts '["a", "b", "c"].none? { |i| i.is_a? String }'
+# puts '=>' + (["a", "b", "c"].none? { |i| i.is_a? String }).to_s
+# puts '["a", "b", "c"].my_none? { |i| i.is_a? String }'
+# puts '=>' + (["a", "b", "c"].my_none? { |i| i.is_a? String }).to_s
+
+# puts "\n"
+# puts '[nil, false, 3].none? '
+# puts '=>' + ([nil, false, 3].none?).to_s
+# puts '[nil, false, 3].my_none? '
+# puts '=>' + ([nil, false, 3].my_none?).to_s
+
+# puts "\n"
+# puts '[nil, false].none? '
+# puts '=>' + ([nil, false].none?).to_s
+# puts '[nil, false].my_none? '
+# puts '=>' + ([nil, false].my_none?).to_s
+
+# puts "\n   *** COUNT ***"
+# puts "\n"
+# puts '[1, 2, 4, 2].count'
+# puts '=>' + ([1, 2, 4, 2].count).to_s
+# puts '[1, 2, 4, 2].my_count'
+# puts '=>' + ([1, 2, 4, 2].my_count).to_s
+
+# puts "\n"
+# puts '[1, 2, 4, 2].count(2)'
+# puts '=>' + ([1, 2, 4, 2].count(2)).to_s
+# puts '[1, 2, 4, 2].my_count(2)'
+# puts '=>' + ([1, 2, 4, 2].my_count(2)).to_s
+
+# puts "\n"
+# puts '[1, 2, 4, 2].count{ |x| x%2==0 }'
+# puts '=>' + ([1, 2, 4, 2].count { |x| x%2==0 }).to_s
+# puts '[1, 2, 4, 2].my_count{ |x| x%2==0 }'
+# puts '=>' + ([1, 2, 4, 2].my_count{ |x| x%2==0 }).to_s
+
+# puts "\n   *** MAP ***"
+# puts "\n"
+# puts '[1, 2, 3, 4].map'
+# puts '=>' + ([1, 2, 3, 4].map).to_s
+# puts '[1, 2, 3, 4].my_map'
+# puts '=>' + ([1, 2, 3, 4].my_map).to_s
+
+# puts "\n"
+# puts '[1, 2, 3, 4].map { |i| i*i }'
+# puts '=>' + ([1, 2, 3, 4].map{ |i| i*i }).to_s
+# puts '[1, 2, 3, 4].my_map { |i| i*i }'
+# puts '=>' + ([1, 2, 3, 4].my_map{ |i| i*i }).to_s
+
+# puts "\n"
+# my_proc = Proc.new { |i| i + i }
+# puts 'my_proc = Proc.new { |i| i + i }'
+
+# puts 'my_map takes proc and block, executes proc'
+# puts '[1, 2, 3, 4].my_map(my_proc)'
+# puts '=>' + ([1, 2, 3, 4].my_map(my_proc)).to_s
+# puts '[1, 2, 3, 4].my_map(my_proc) { |i| i*i }'
+# puts '=>' + ([1, 2, 3, 4].my_map(my_proc) { |i| i*i }).to_s
+
+
+puts "\n   *** INJECT ***"
+puts "\n"
+arr = [1, 2, 3, 4]
 puts "arr = #{arr}"
+puts "\n"
+puts 'arr.inject { |memo, obj| memo * obj }'
+puts (arr.inject { |memo, obj| memo * obj })
+puts 'arr.my_inject { |memo, obj| memo * obj}'
+puts (arr.my_inject { |memo, obj| memo * obj })
 
-puts "\n   *** ALL? ***"
-puts 'arr.all? { |i| i.is_a? Integer }'
-p(arr.all? { |i| i.is_a? Integer })
+puts "\n"
+puts 'arr.inject(0.5) { |memo, obj| memo * obj}'
+puts (arr.inject(0.5) { |memo, obj| memo * obj })
+puts 'arr.my_inject(0.5) { |memo, obj| memo * obj}'
+puts (arr.my_inject(0.5) { |memo, obj| memo * obj })
 
-puts "\n   *** MY_ALL? ***"
-puts 'arr.my_all? { |i| i.is_a? Integer }'
-p(arr.my_all? { |i| i.is_a? Integer })
+puts "\n"
+puts 'arr.inject(:*)'
+puts arr.inject(:*)
+puts 'arr.my_inject(:*)'
+puts arr.my_inject(:*)
 
-arr = [1, 2, 3]
-puts "arr = #{arr}"
+puts "\n"
+puts 'arr.inject(0.5, :*)'
+puts arr.inject(0.5, :*)
+puts 'arr.my_inject(0.5, :*)'
+puts arr.my_inject(0.5, :*)
 
-puts "\n   *** ALL? ***"
-puts 'arr.all? { |i| i.is_a? Integer }'
-p(arr.all? { |i| i.is_a? Integer })
+hsh = { a: 1, b: 2, c: 3 }
 
-puts "\n   *** MY_ALL? ***"
-puts 'arr.my_all? { |i| i.is_a? Integer }'
-p(arr.my_all? { |i| i.is_a? Integer })
-
-
-# puts '*** #any test ***'
-# p(arr.my_any? { |i| i.is_a? Integer })
-# p(arr.any? { |i| i.is_a? Integer })
-
-# puts '*** #none? test ***'
-# p(arr.my_none? { |i| i.is_a? Integer })
-# p(arr.none? { |i| i.is_a? Integer })
-
-
+# puts 'hsh.inject(0) { |sum, (k, v)| sum + v }'
+# p hsh.inject(0) { |sum, (k, v)| sum + v } 
+# # binding.pry
+# p hsh.my_inject(0) { |sum, (k, v)| sum + v } 
